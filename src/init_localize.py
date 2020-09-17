f21257c3b5b3beaf1ddeef5c9fa9f9c84200177f
Corey Williams <corey.williams3@rockets.utoledo.edu>
2020-09-17 15:42:49 -0400

    added global localization

--- src/init_localize.py
+++ src/init_localize.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
 from std_srvs.srv import Empty
 
 
 def main():
     rospy.init_node("init_localize")
+    rospy.sleep(2)
     rospy.wait_for_service("global_localization")
     try:
         localize = rospy.ServiceProxy("global_localization", Empty)
         req = localize()
+        rospy.loginfo("Calling global_localization service")
     except rospy.ServiceException as e:
-        rospy.log_warn("Service call failed: %s" % e)
+        rospy.logwarn("Service call failed: %s" % e)
 
 
 if __name__=="__main__":
